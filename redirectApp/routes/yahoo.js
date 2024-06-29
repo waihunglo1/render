f65884c17810d8ca2ab23ae2f4fad6c0ec596f20
stayinvest-for-dummy <waihunglo@gmail.com>
2024-06-30 00:26:42 +0800

    yahoo

--- redirectApp/routes/yahoo.js
+++ redirectApp/routes/yahoo.js
@@ -1,32 +1,95 @@
 var express = require('express');
 var router = express.Router();
-// require syntax (if your code base does not support imports)
+const moment = require('moment');
+const taIndicator = require('@debut/indicators');
+const yahooFinance = require('yahoo-finance2').default;
 
-var yahooFinance = require('yahoo-finance');
-// var utils = require('./utils');
-
-/*
-utils.show(async () => {
-  const result = await yahooFinance.quoteSummary("TSLA", {
-    // 1. Try adding, removing or changing modules
-    // You'll get suggestions after typing first quote mark (")
-    modules: ["price"]
-  });
-  */
-
-  // 2. Put a "." after result to explore
-  // return result;
-
-  // 3. Try change "quoteSummary" above to something else
-//});
-
-
-
-/* GET users listing. */
+/**
+ * 
+ */
 router.get('/', function (req, res, next) {
-  res.send('respond with a yahoo');
+  // previous 25 days
+  var targetDate = moment().subtract(25, "days");
+  var todayStr = targetDate.format("YYYY-MM-DD");
+  const stockCodesStr = req.query.cgo;
+  var stockCodes = [];
 
+  console.log("date:" + todayStr + " cgo:" + stockCodesStr);  
+  if (!empty(stockCodesStr)) {
+    stockCodes = stockCodesStr.split(",");
+  } 
 
+  queryMultipleStockTechIndicator(todayStr, stockCodes)
+    .then(function (stocks) {
+      // res.send(stocks);
+      res.json(stocks);
+    });
 });
+
+/**
+ * 
+ * @param {*} todayStr 
+ * @param {*} stockCodes 
+ * @returns 
+ */
+const queryMultipleStockTechIndicator = async (todayStr, stockCodes) => {
+  var bars = [];
+  var stockList = [];
+
+  stockCodes.forEach(code => {
+    var bar = new Promise((resolve, reject) => {
+      var stock = {
+        "symbol": code,
+        "extra": 0
+      }
+      queryHistoryPrices(todayStr, stock)
+        .then(function () {
+          stockList.push(stock);
+          resolve(stock);
+        });
+    });
+
+    bars.push(bar);
+  });
+
+  await Promise.all(bars)
+    .then((values) => {
+      console.log(values);
+  });
+
+  return stockList;
+}
+
+/**
+ * 
+ * @param {*} todayStr 
+ * @param {*} stockCodeStr 
+ * @returns 
+ */
+const queryHistoryPrices = async (todayStr, stock) => {
+  // format query options
+  const queryOptions = { period1: todayStr, /* ... */ };
+  const stockPrices = await yahooFinance.historical(stock.symbol, queryOptions);
+
+  // create tech indicator
+  const sma = new taIndicator.SMA(10);
+  const rsi = new taIndicator.RSI(12);
+  const roc = new taIndicator.ROC(12);
+
+  // calculate
+  stockPrices.forEach((row, idx) => {
+    stock.extra = roc.nextValue(row.adjClose);
+  });
+
+  return stock;
+}
+
+function empty(str)
+{
+    if (typeof str == 'undefined' || !str || str.length === 0 || str === "" || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") === "")
+        return true;
+    else
+        return false;
+}
 
 module.exports = router;
